5374df194fb9efa02d485cf841b2f6cfa9f43150
ana luisa <140508741+analuiisasch@users.noreply.github.com>
2023-10-03 11:52:44 -0300

    Add files via upload

---
+++ typenumber.js
@@ -0,0 +1,23 @@
+//tipo number
+
+const primeironumero =1;
+const segundonumero =2;
+
+const operaçãomatematica = primeironumero + segundonumero;
+
+console.log(operaçãomatematica);
+
+//ponto flutuante
+
+const numeroflutuante1 = 0.3;
+const numeroflutuante2=0.5;
+
+const novaoperação= primeironumero / numeroflutuante1;
+
+console.log(novaoperação);
+
+//.nannot a number (não e um numero)
+
+//const testnan= "teste nan";
+
+//console.log(primeironumero*testenan);
